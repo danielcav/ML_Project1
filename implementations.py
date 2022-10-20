62f1eb109a1284ca93111661c459303c5b7b3496
danielcav <daniel.cavaleri@epfl.ch>
2022-10-20 17:04:19 +0200

    small changes in logistic loss

--- implementations.py
+++ implementations.py
@@ -36,7 +36,7 @@ def compute_logistic_gradient(y, tx, w):
 
 
 def compute_logistic_loss(y, tx, w):
-    model = tx.T.dot(w)
+    model = tx.dot(w)
     # loss = y.T.dot(np.log(pred)) + (1 - y).T.dot(np.log(1 - pred))
     loss = np.sum(np.log(1 + np.exp(model)) - y.dot(model), axis=0)
     return np.squeeze(loss)
